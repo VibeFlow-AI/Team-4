60c744c3d9a5e0078f4077130fed564647a52707
Rashad Sirajudeen <rashad@wso2.com>
2025-07-19 10:36:10 +0530

    hero and navbar ok

--- front-end/src/pages/home.tsx
+++ front-end/src/pages/home.tsx
@@ -1,11 +1,16 @@
+
+import { Navbar } from "@/components/Navbar";
+import Hero from "@/components/home/hero";
+
 function HomePage() {
-
   return (
-    <div>hehe</div>
-  )
-
-  
-  
+    <div className="min-h-screen" style={{ backgroundColor: '#F4F4F4' }}>
+      <Navbar />
+      <Hero />
+      
+      
+    </div>
+  );
 }
 
 export default HomePage;
